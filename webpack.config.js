2e6d65ca5c38708d56cbfa238139b649ce4b86b4
csaba.daniel@db-soft.hu <csaba.daniel@db-soft.hu>
2019-12-22 20:31:28 +0000

    Add w3.css

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
       },
       {
-        test: /\.html/,
+        test: /\.html$/,
         use: {
           loader: 'html-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
